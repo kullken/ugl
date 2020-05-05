e06c782a2893821aaee793a2eb86de988475081a
Viktor <viktorku@kth.se>
2020-05-05 16:53:50 +0200

    exp() and log() for quaternions.

--- include/geometry_cpp/math/quaternion.h
+++ include/geometry_cpp/math/quaternion.h
@@ -9,4 +9,10 @@ namespace geometry::math
 using Quaternion = Eigen::Quaternion<double>;
 using UnitQuaternion = Eigen::Quaternion<double>;
 
+/// Calculates the exponential of an imaginary-only quaternion.
+UnitQuaternion exp(const Quaternion& q);
+
+/// Calculates the natural logarithm of a unit-norm quaternion.
+Quaternion log(const UnitQuaternion& q);
+
 }
--- src/math/quaternion.cpp
+++ src/math/quaternion.cpp
@@ -1,8 +1,30 @@
 #include "math/quaternion.h"
 
+#include <cassert>
+#include <cmath>
+
+#include "math/vector.h"
 
 namespace geometry::math
 {
 
+UnitQuaternion exp(const Quaternion& q)
+{
+    [[maybe_unused]] constexpr double tolerance = 1e-6;
+    assert(("Imaginary quaternion required.", std::abs(q.w()) < tolerance));
+    Vector3 u = q.vec();
+    double u_norm = u.norm();
+    Vector3 v = u.normalized() * std::sin(u_norm);
+    return Quaternion(std::cos(u_norm), v.x(), v.y(), v.z());
+}
+
+Quaternion log(const UnitQuaternion& q)
+{
+    [[maybe_unused]] constexpr double tolerance = 1e-6;
+    assert(("Unit-norm quaternion required.", std::abs(q.norm() - 1) < tolerance));
+    Vector3 u = q.vec();
+    Vector3 v = u.normalized() * std::acos(q.w());
+    return Quaternion(0, v.x(), v.y(), v.z());
+}
 
 }
