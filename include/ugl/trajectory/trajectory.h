afdd84860e4cb6f55c88fa7ccb8809655948adcb
Viktor <viktorku@kth.se>
2020-05-11 11:58:09 +0200

    Implement getters for top-level trajectory.

--- include/ugl/trajectory/trajectory.h
+++ include/ugl/trajectory/trajectory.h
@@ -20,12 +20,12 @@ public:
 
     double duration() const { return std::min(m_linear_trajectory.duration(), m_angular_trajectory.duration()); }
 
-    math::Vector3 get_position(double t) const;
-    math::Vector3 get_velocity(double t) const;
-    math::Vector3 get_acceleration(double t) const;
+    math::Vector3 get_position(double t) const { return m_linear_trajectory.get_position(t); }
+    math::Vector3 get_velocity(double t) const { return m_linear_trajectory.get_velocity(t); }
+    math::Vector3 get_acceleration(double t) const { return m_linear_trajectory.get_acceleration(t); }
 
-    math::Rotation get_rotation(double t) const;
-    math::Vector3 get_angular_velocity(double t) const;
+    math::Rotation get_rotation(double t) const { return m_angular_trajectory.get_rotation(t); }
+    math::Vector3 get_angular_velocity(double t) const { return m_angular_trajectory.get_angular_velocity(t); }
 };
 
 }
