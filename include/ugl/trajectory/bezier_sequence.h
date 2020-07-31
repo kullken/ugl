fa2689f87cfa9d37d275657ee435b3287e0c429c
kullken <viktorku@kth.se>
2020-07-31 16:25:59 +0200

    Refactor BezierSequence into template<int degree>.

--- include/ugl/trajectory/bezier_sequence.h
+++ include/ugl/trajectory/bezier_sequence.h
@@ -1,7 +1,9 @@
 #pragma once
 
-#include <vector>
+#include <cassert>
 #include <memory>
+#include <stdexcept>
+#include <vector>
 
 #include "ugl/math/vector.h"
 
@@ -11,23 +13,30 @@
 namespace ugl::trajectory
 {
 
-// TODO?: Template with degree parameter?
+template<int degree>
 class BezierSequence : public LinearTrajectory
 {
-    class Segment : public Bezier<3>
+    class Segment : public Bezier<degree>
     {
     public:
         double time_offset;
+        Segment(const Bezier<degree>& bezier, double offset) 
+            : Bezier<degree>(bezier)
+            , time_offset(offset) {}
     };
-
-private:
-    double duration_ = 0;
-    std::vector<Segment> segments_;
 
 public:
     BezierSequence() = default;
 
-    explicit BezierSequence(const std::vector<Segment>& segments);
+    explicit BezierSequence(const std::vector<Bezier<degree>>& beziers)
+    {
+        for (const auto& bezier : beziers)
+        {
+            segments_.emplace_back(bezier, duration_);
+            duration_ += bezier.duration();
+        }
+        // TODO: Assert that all segments are connected continously in position, velocity(?), acceleration(??) and jerk(???).
+    }
 
     std::unique_ptr<LinearTrajectory> clone() const override
     {
@@ -36,12 +45,43 @@ public:
 
     double duration() const override { return duration_; }
 
-    math::Vector3 pos(double t) const override;
-    math::Vector3 vel(double t) const override;
-    math::Vector3 acc(double t) const override;
+    math::Vector3 pos(double t) const override
+    {
+        const Segment& segment = get_segment_at(t);
+        return segment.pos(t - segment.time_offset);
+    }
+
+    math::Vector3 vel(double t) const override
+    {
+        const Segment& segment = get_segment_at(t);
+        return segment.vel(t - segment.time_offset);
+    }
+
+    math::Vector3 acc(double t) const override
+    {
+        const Segment& segment = get_segment_at(t);
+        return segment.acc(t - segment.time_offset);
+    }
 
 private:
-    const Segment& get_segment_at(double t) const;
+    const Segment& get_segment_at(double t) const
+    {
+        assert(0 <= t && t <= duration_);
+
+        for (auto it = std::rbegin(segments_); it != std::rend(segments_); ++it)
+        {
+            if (it->time_offset <= t)
+            {
+                return *it;
+            }
+        }
+
+        throw std::logic_error("Could not access trajectory segment."); 
+    }
+
+private:
+    double duration_ = 0;
+    std::vector<Segment> segments_;
 };
 
 }
--- src/trajectory/bezier_sequence.cpp
+++ src/trajectory/bezier_sequence.cpp
@@ -1,56 +1,6 @@
 #include "ugl/trajectory/bezier_sequence.h"
 
-#include <vector>
-#include <cassert>
-#include <stdexcept>
-
-#include "ugl/math/vector.h"
-
 namespace ugl::trajectory
 {
-
-BezierSequence::BezierSequence(const std::vector<Segment>& segments)
-    : segments_(segments)
-{
-    for (auto& segment : segments_)
-    {
-        segment.time_offset = duration_;
-        duration_ += segment.duration();
-    }
-    // TODO?: Assert that all segments are connected continously up to the x(?):th derivative.
-}
-
-math::Vector3 BezierSequence::pos(double t) const
-{
-    const Segment& segment = get_segment_at(t);
-    return segment.pos(t - segment.time_offset);
-}
-
-math::Vector3 BezierSequence::vel(double t) const
-{
-    const Segment& segment = get_segment_at(t);
-    return segment.vel(t - segment.time_offset);
-}
-
-math::Vector3 BezierSequence::acc(double t) const
-{
-    const Segment& segment = get_segment_at(t);
-    return segment.acc(t - segment.time_offset);
-}
-
-const BezierSequence::Segment& BezierSequence::get_segment_at(double t) const
-{
-    assert(0 <= t && t <= duration_);
-
-    for (auto it = std::rbegin(segments_); it != std::rend(segments_); ++it)
-    {
-        if (it->time_offset <= t)
-        {
-            return *it;
-        }
-    }
-
-    throw std::logic_error("Could not access trajectory segment."); 
-}
 
 }
