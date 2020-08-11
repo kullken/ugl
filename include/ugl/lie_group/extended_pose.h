6503773794c45f246fe47a0fbe701cfee624030f
kullken <viktorku@kth.se>
2020-08-11 14:07:15 +0200

    Create type for Lie group SE2(3).

---
+++ include/ugl/lie_group/extended_pose.h
@@ -0,0 +1,117 @@
+#pragma once
+
+#include "ugl/math/vector.h"
+#include "ugl/math/matrix.h"
+
+#include "ugl/lie_group/rotation.h"
+
+namespace ugl::lie
+{
+
+// Lie-algebra of SE2(3).
+// TODO: Create unique class?
+using se2_3 = ugl::Matrix<5,5>;
+
+// The group of extended poses {R,v,p}. Also known as SE2(3). The group is a matrix Lie group.
+class ExtendedPose
+{
+public:
+    ExtendedPose() = default;
+
+    explicit ExtendedPose(const Matrix<5,5>& matrix)
+        : R_(matrix.block<3,3>(0,0))
+        , vel_(matrix.block<3,1>(0,3))
+        , pos_(matrix.block<3,1>(0,4))
+    {
+    }
+
+    ExtendedPose(const Rotation& rotation, const Vector3& velocity, const Vector3& position)
+        : R_(rotation)
+        , vel_(velocity)
+        , pos_(position)
+    {
+    }
+
+    Matrix<5,5> matrix() const
+    {
+        Matrix<5,5> matrix = Matrix<5,5>::Identity();
+        matrix.block<3,3>(0,0) = R_.matrix();
+        matrix.block<3,1>(0,3) = vel_;
+        matrix.block<3,1>(0,4) = pos_;
+        return matrix;
+    }
+
+    const Rotation& rotation() const { return R_; }
+
+    const Vector3& velocity() const { return vel_; }
+
+    const Vector3& position() const { return pos_; }
+
+    // Group identity element.
+    static ExtendedPose Identity() 
+    {
+        return ExtendedPose{};
+    }
+
+    // Returns the inverse. The inverse is equal to the transpose for valid rotation matrices.
+    ExtendedPose inverse() const
+    {
+        const Rotation Rinv = R_.inverse();
+        return ExtendedPose{Rinv, -(Rinv * vel_), -(Rinv * pos_)};
+    }
+
+    // Group composition
+    ExtendedPose& operator*=(const ExtendedPose& rhs)
+    {
+        vel_ += R_ * rhs.vel_;
+        pos_ += R_ * rhs.pos_;
+        R_ *= rhs.R_; // Must be last since R_ is used above.
+        return *this;
+    }
+
+    // Maps R^9 -> SE2(3).
+    static ExtendedPose exp(const Vector<9>& u);
+
+    // Maps SE2(3) -> R^9.
+    static Vector<9> log(const ExtendedPose& T);
+
+    // Maps R^9 -> se2(3). Also known as 'wedge' or '^'-operator.
+    static se2_3 hat(const Vector<9>& u);
+
+    // Maps se2(3) -> R^9. Is the local inverse of the hat operator.
+    static Vector<9> vee(const se2_3& U);
+
+    // Returns the adjoint matrix of an extended pose.
+    static Matrix<9,9> adjoint(const ExtendedPose& T);
+
+private:
+    // Rotation
+    Rotation R_ = Rotation::Identity();
+    // Velocity
+    Vector3 vel_ = Vector3::Zero();
+    // Position
+    Vector3 pos_ = Vector3::Zero();
+};
+
+inline
+ExtendedPose operator*(ExtendedPose lhs, const ExtendedPose& rhs)
+{
+    return lhs *= rhs;
+}
+
+inline
+Matrix<5,5> operator*(const ExtendedPose& lhs, const Matrix<5,5>& rhs)
+{
+    return lhs.matrix() * rhs;
+}
+
+inline
+Matrix<5,5> operator*(const Matrix<5,5>& lhs, const ExtendedPose& rhs)
+{
+    return lhs * rhs.matrix();
+}
+
+// An alias to make math nerds happy.
+using SE2_3 = ExtendedPose;
+
+} // namespace ugl::lie
---
+++ src/lie_group/extended_pose.cpp
@@ -0,0 +1,55 @@
+#include "ugl/lie_group/extended_pose.h"
+
+#include "ugl/math/vector.h"
+#include "ugl/math/matrix.h"
+
+#include "ugl/lie_group/rotation.h"
+
+namespace ugl::lie
+{
+
+ExtendedPose ExtendedPose::exp(const Vector<9>& u)
+{
+    // TODO: Analytical solution
+    return ExtendedPose{math::exp(hat(u))};
+}
+
+Vector<9> ExtendedPose::log(const ExtendedPose& T)
+{
+    // TODO: Analytical solution
+    return vee(math::log(T.matrix()));
+}
+
+se2_3 ExtendedPose::hat(const Vector<9>& u)
+{
+    se2_3 U = se2_3::Zero();
+    U.block<3,3>(0,0) = SO3::hat(u.segment<3>(0));
+    U.block<3,1>(0,3) = u.segment<3>(3);
+    U.block<3,1>(0,4) = u.segment<3>(6);
+    return U;
+}
+
+Vector<9> ExtendedPose::vee(const se2_3& U)
+{
+    Vector<9> u;
+    u << SO3::vee(U.block<3,3>(0,0)), U.block<3,1>(0,3), U.block<3,1>(0,4);
+    return u;
+}
+
+Matrix<9,9> ExtendedPose::adjoint(const ExtendedPose& T)
+{
+    const Matrix3& R = T.R_.matrix();
+    const Vector3&  v = T.vel_;
+    const Vector3&  p = T.pos_;
+
+    Matrix<9,9> Adj = Matrix<9,9>::Zero();
+    Adj.block<3,3>(0,0) = R;
+    Adj.block<3,3>(3,3) = R;
+    Adj.block<3,3>(6,6) = R;
+    Adj.block<3,3>(3,0) = SO3::hat(v) * R;
+    Adj.block<3,3>(6,0) = SO3::hat(p) * R;
+
+    return Adj;
+}
+
+} // namespace ugl::lie
