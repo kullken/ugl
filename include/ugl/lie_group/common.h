245055c1f623d9acd29200e4e3929ce55c37e9d8
kullken <karl.viktor.kull@gmail.com>
2024-01-06 13:39:03 +0100

    feat: Interpolation on Lie groups

--- include/ugl/lie_group/common.h
+++ include/ugl/lie_group/common.h
@@ -36,6 +36,18 @@ typename LieGroup::VectorType ominus(const LieGroup& Y, const LieGroup& X)
     return LieGroup::log(X.inverse() * Y);
 }
 
+/// @brief Interpolate between two values.
+/// @param start Start value of the interpolation
+/// @param end End value of the interpolation
+/// @param ratio The ratio of interpolation. A ratio outside interval [0,1] will result in
+/// extrapolation.
+/// @return The interpolated value
+template <typename LieGroup>
+LieGroup interpolate(const LieGroup &start, const LieGroup &end, double ratio)
+{
+    return oplus(start, ratio * ominus(end, start));
+}
+
 } // namespace ugl::lie
 
 #endif // UGL_LIE_COMMON_H
